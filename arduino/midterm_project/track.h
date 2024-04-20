f132d638fed23c92b118c666f63400d95d53f7ce
Ivantung <ypt0708@gmail.com>
2024-04-20 09:18:13 +0800

    Finish Tracking

--- arduino/midterm_project/track.h
+++ arduino/midterm_project/track.h
@@ -16,29 +16,82 @@ int extern _Tp;
 
 // Write the voltage to motor.
 void MotorWriting(double vL, double vR) {
-    // TODO: use TB6612 to control motor voltage & direction
+  if (vL > 0.0) {
+    digitalWrite(MotorL_I3, HIGH);
+    digitalWrite(MotorL_I4, LOW);
+  } else {
+    digitalWrite(MotorL_I3, LOW);
+    digitalWrite(MotorL_I4, HIGH);
+    vL = -vL;
+  }
+  if (vR > 0.0) {
+    digitalWrite(MotorR_I1, HIGH);
+    digitalWrite(MotorR_I2, LOW);
+  } else {
+    digitalWrite(MotorR_I1, LOW);
+    digitalWrite(MotorR_I2, HIGH);
+    vR = -vR;
+  }
+  analogWrite(MotorR_PWMR, vR);
+  analogWrite(MotorL_PWML, vL);
+
 }  // MotorWriting
 
-// Handle negative motor_PWMR value.
-void MotorInverter(int motor, bool& dir) {
-    // Hint: the value of motor_PWMR must between 0~255, cannot write negative value.
-}  // MotorInverter
-
+void reverse() {
+    delay(500);
+    MotorWriting(255,-255);
+    delay(300);
+    while(!digitalRead(IRpin_M)) {
+        MotorWriting(150,-150);
+        delay(10);
+    } 
+}
+void right_turn() {
+    delay(500); // Wait until the car goes into the center
+    MotorWriting(255,-255);
+    delay(200);
+    while(!digitalRead(IRpin_M) || digitalRead(IRpin_LL) || digitalRead(IRpin_RR)) {
+        MotorWriting(100,-100);
+        delay(10);
+    }
+}
 // P/PID control Tracking
-void tracking(int l2, int l1, int m0, int r1, int r2) {
-    // TODO: find your own parameters!
-    double _w0;  //
-    double _w1;  //
-    double _w2;  //
-    double _Kp;  // p term parameter
-    double _Kd;  // d term parameter (optional)
-    double _Ki;  // i term parameter (optional) (Hint: 不要調太大)
-    double error = l2 * _w2 + l1 * _w1 + m0 * _w0 + r1 * (-_w1) + r2 * (-_w2);
-    double vR, vL;  // 馬達左右轉速原始值(從PID control 計算出來)。Between -255 to 255.
-    double adj_R = 1, adj_L = 1;  // 馬達轉速修正係數。MotorWriting(_Tp,_Tp)如果歪掉就要用參數修正。
+int weight[5] = { -2, -1, 0, 1, 2 };
+int sensors[5] = { IRpin_LL, IRpin_L, IRpin_M, IRpin_R, IRpin_RR };
+void tracking() {
+  // TODO: find your own parameters!
+  double delta = 0;
+  int high_num = 0;
+  for (int i = 0; i < 5; i++) {
+    delta += weight[i] * digitalRead(sensors[i]);
+    high_num += digitalRead(sensors[i]);
+  }
+  delta /= high_num;
+  if (delta > 1) {
+    MotorWriting(200, -200);
+    return;
+  }
+  if (delta < -1) {
+    MotorWriting(-200, 200);
+    return;
+  }
+  MotorWriting(150 + 80 * delta, 150);
+  /*int l2 = digitalRead(IRpin_LL);
+  int l1 = digitalRead(IRpin_L);
+  int m = digitalRead(IRpin_M);
+  int r1 = digitalRead(IRpin_R);
+  int r2 = digitalRead(IRpin_RR);
+  double _w0;  //
+  double _w1;  //
+  double _w2;  //
+  double _Kp;  // p term parameter
+  double _Kd;  // d term parameter (optional)
+  double _Ki;  // i term parameter (optional) (Hint: 不要調太大)
+  double error = l2 * _w2 + l1 * _w1 + m * _w0 + r1 * (-_w1) + r2 * (-_w2);
+  double vR, vL;                // 馬達左右轉速原始值(從PID control 計算出來)。Between -255 to 255.
+  double adj_R = 1, adj_L = 1;  // 馬達轉速修正係數。MotorWriting(_Tp,_Tp)如果歪掉就要用參數修正。
+  // TODO: complete your P/PID tracking code
 
-    // TODO: complete your P/PID tracking code
-
-    // end TODO
-    MotorWriting(adj_L * vL, adj_R * vR);
+  // end TODO
+  //MotorWriting(adj_L * vL, adj_R * vR);*/
 }  // tracking
